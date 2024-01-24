a3c298cb661088e2b5a1ff6479c56042cf5bfe6c
ushabanda <122169198+ushabanda@users.noreply.github.com>
2024-01-24 15:55:39 +0530

    Delete parser.py

--- parser.py
+++
@@ -1,9 +0,0 @@
-
-from resume_parser import resumeparse
-
-
-
-# data = resumeparse.read_file('Naukri_Deepika.docx')
-data = resumeparse.read_file('Ashok_resume.pdf')
-
-print("\n\n ============ \n\n",data)
